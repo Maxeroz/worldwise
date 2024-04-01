7ac04bfbafc8787d4579de4cd56299b37a251401
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-04-01 19:39:57 +0400

    Building the App Layout

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
